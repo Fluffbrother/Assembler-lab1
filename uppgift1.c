bf5467373edc6ce1c02b1eaf7b5d174cbbfa6696
Fluffbrother <danielv.brynne@gmail.com>
2024-09-23 15:54:25 +0200

    Uppgift 1 och början på uppgift 4

---
+++ uppgift1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define MIN_IN 'b'
+#define MAX_IN 'y'
+
+int main (){
+    char input;
+    scanf("%c", &input);
+
+    if(input >= MIN_IN && input <= MAX_IN){
+        char prev = input-1;
+        char next = input+1;
+        printf("%c %c\n", prev, next);
+        return 0;
+    }
+
+    return 1;
+}
