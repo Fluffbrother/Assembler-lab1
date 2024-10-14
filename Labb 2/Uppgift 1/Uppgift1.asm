73e60caf2515693c1177349a390816d691d64244
Fluffbrother <danielv.brynne@gmail.com>
2024-10-14 11:04:23 +0200

    Påbörjat översättning

--- Labb 2/Uppgift 1/Uppgift1.asm
+++ Labb 2/Uppgift 1/Uppgift1.asm
@@ -1,5 +1,25 @@
     .data
+antal: .word 10    
+vek: .word 4, 5, 2, 2, 1, 6, 7, 9, 5, 10
 
     .text
-    .globl main
+merge: 
+    lw $t0, antal
+    la $t1, vek
+
+    subu $sp, $sp, 16
+
+
+loop:
+    addi $t2,$t2,1
+    bgt $t2, $t0, exit
+
+
+
+mergeSort:
+
+printArray:
+
 main: 
+
+exit: 
